9cb513c2c05ab58039a85002f2d61fde773d637a
Raquel <raqueelima1@gmail.com>
2024-07-30 16:32:54 -0300

    titulo

--- ProgramaBiblioteca.java
+++ ProgramaBiblioteca.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class ProgramaBiblioteca {
     
     public static void main(String[] args) {
-        Livro objLivro = new Livro("Senhor dos Aneis", "Tanan da Silva", 2024, "terror", "aaa", 2, 25637, 2);
+        Livro objLivro = new Livro("Senhor dos A", "Tanan da Silva", 2024, "terror", "aaa", 2, 25637, 2);
         Filmes objFilmes = new Filmes("Harry Potter", "Fulano", 2014, "Aventura", "Globo", 5, "Ciclano", "Harry", 120, "Um jovem magico...");
         
 
